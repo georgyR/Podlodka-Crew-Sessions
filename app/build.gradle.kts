78b98054febfd9b79d05a12fcac6205f8bae8b15
g.ryabykh <georgy.ryabykh@gmail.com>
2021-07-20 22:20:03 +0300

    update dependencies versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,14 +46,20 @@ android {
 dependencies {
     val composeVersion = rootProject.extra["compose_version"]
 
-    implementation("androidx.core:core-ktx:1.5.0")
+    implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.3.0")
+    implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
-    implementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling:$composeVersion") {
+        version {
+            // Remove this when Android Studio has become compatible again
+            // https://stackoverflow.com/questions/68224361/jetpack-compose-cant-preview-after-updating-to-1-0-0-rc01
+            strictly("1.0.0-beta09")
+        }
+    }
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.0-rc01")
-    implementation("com.google.accompanist:accompanist-coil:0.12.0")
+    implementation("androidx.activity:activity-compose:1.3.0-rc02")
+    implementation("com.google.accompanist:accompanist-coil:0.14.0")
     implementation("androidx.compose.runtime:runtime-livedata:$composeVersion")
 }
