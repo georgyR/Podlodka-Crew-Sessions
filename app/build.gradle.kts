61e2d4f0797202c4e1fc1c07cb48a593dfb55ed6
g.ryabykh <georgy.ryabykh@gmail.com>
2021-12-10 19:19:32 +0300

    update libraries version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,16 +3,17 @@ plugins {
     id("kotlin-android")
 }
 
-val composeVersion = "1.0.1"
+val composeVersion = "1.0.5"
 
 android {
-    compileSdk = 30
+    namespace = "com.georgy_r.podlodkaandroidcrew"
+    compileSdk = 31
     buildToolsVersion = "30.0.3"
 
     defaultConfig {
         applicationId = "com.georgy_r.podlodkaandroidcrew"
         minSdk = 23
-        targetSdk = 30
+        targetSdk = 31
         versionCode = 1
         versionName = "1.0"
 
@@ -44,15 +45,15 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.material:material-icons-extended:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling:$composeVersion")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
+    implementation("androidx.activity:activity-compose:1.4.0")
     implementation("com.google.accompanist:accompanist-coil:0.14.0")
     implementation("androidx.compose.runtime:runtime-livedata:$composeVersion")
 }
